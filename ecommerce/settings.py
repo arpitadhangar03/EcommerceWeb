6e174a8692f168688e6a6b240f51ccd17e643164
Arpita Dhangar <arpitadhangar03@gmail.com>
2025-12-25 13:04:53 +0530

    update settings for render deployment

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -14,9 +14,9 @@ from pathlib import Path
 import os
 
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY','fallback-secret-key')
 
-DEBUG = os.environ.get('DEBUG') == 'True'
+DEBUG = os.environ.get('DEBUG','False') == 'True'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4ifdmltl+dahs1-(ch3ta#3z((fs#4hm^i)-l#hhz!!h+pv2a^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
 
 ALLOWED_HOSTS = ['*']
 
